a5996de34a53984f015217746bc72f2e212ba446
marseille78 <boyko.ruslan78@gmail.com>
2017-11-09 00:04:33 +0200

    2017.11.08: JS. autocomplete

--- autocomplete_from_list/script.js
+++ autocomplete_from_list/script.js
@@ -1,32 +1,40 @@
-function ListAutocomplete(selector, nameInput, classWrap) {
-    this.selector = document.querySelector(selector) || null;
-    this.nameInput = nameInput || '';
-    this.classWrap = classWrap || '';
-
-    // function createField() {
-    //     var wrap = document.createElement('div');
-    //     var field = document.createElement('input');
-    //     wrap.classList.add(this.classWrap);
-    //     field.setAttribute('type', 'text');
-    //     field.setAttribute('name', this.nameInput);
-    //     wrap.appendChild(field);
-    //     document.insertBefore(wrap, this.selector);
-    // }
-}
-ListAutocomplete.prototype._createField = function() {
-    debugger;
-    var wrap = document.createElement('div');
-    var field = document.createElement('input');
-    field.setAttribute('type', 'text');
-    field.setAttribute('name', this.nameInput);
-    wrap.appendChild(field);
-    document.insertBefore(wrap, this.selector);
-}.bind(this);
-
-ListAutocomplete.prototype.init = function() {
-    this._createField();
+// Паттерн Фабрика
+/*
+function createPerson(name, age, job) {
+    var o = new Object();
+    o.name = name;
+    o.age = age;
+    o.job = job;
+    o.sayName = function() {
+        console.log(this.name);
+    };
+    return o;
 }
 
-var listAutocomplete = new ListAutocomplete('.autocomplete2');
+var person1 = createPerson('Nicholas', 29, 'Software Engineer');
+var person2 = createPerson('Greg', 27, 'Doctor');
 
-listAutocomplete.init();
+console.log(person1);
+console.log(person2);
+*/
+
+
+
+// Паттерн Конструктор
+function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function() {
+        console.log(this.name);
+    }
+}
+
+var person1 = new Person('Nicholas', 29, 'Software Engineer');
+var person2 = new Person('Greg', 27, 'Doctor');
+
+console.log(person1);
+console.log(person2);
+
+console.log(person1.constructor == Person); // true
+console.log(person2.constructor == Person); // true
